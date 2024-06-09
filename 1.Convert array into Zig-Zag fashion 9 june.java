d4ac1f8ae324b5efa2172fe94cf543a1152548fb
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-09 08:30:31 +0530

    Update 1.Convert array into Zig-Zag fashion 9 june.java

--- 1.Convert array into Zig-Zag fashion 9 june.java
+++ 1.Convert array into Zig-Zag fashion 9 june.java
@@ -1,5 +1,9 @@
 class Solution {
     public static void zigZag(int n, int[] arr) {
+        //This Code in Comments is written by me when attempting code
+        //In this code we have to written Something that was creating differnce in output
+        //In this code we do not have to return something.
+        
         // int curr=1;
         // int flag=0;
         // while(curr!=n){
@@ -26,7 +30,13 @@ class Solution {
         //         }
         //     }
         // }
-    
+
+       //What is the differnce Between this Code and code written downwards
+        //1.They have changed flag to Boolean unlike me to int 
+        //2. They dont have used current variabe in the code instead they have run a for loop
+
+
+        
         // Use a flag to alternate between "<" and ">"
         boolean flag = true;  // Start with the condition arr[0] < arr[1]
 
