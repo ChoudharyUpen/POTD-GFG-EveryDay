2f76fd22bb6dee7676d54c1ae23fd776b2f4e7aa
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-10 07:16:44 +0530

    Create 2.Nuts and Bolts Problem 10 june.java

---
+++ 2.Nuts and Bolts Problem 10 june.java
@@ -0,0 +1,10 @@
+//WTF question was this was only used 2 array.sort and it passed all testcases
+//no way this was very easy and they have termed this as medium wtf
+
+
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        Arrays.sort(nuts);
+        Arrays.sort(bolts);
+    }
+}
